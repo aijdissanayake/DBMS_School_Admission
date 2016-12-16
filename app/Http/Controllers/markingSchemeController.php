c1331b99a33b5c5e9f367117b97e0523d0b4e1f2
Lahiru de Alwis <lahirud.14@cse.mrt.ac.lk>
2016-12-16 18:07:29 +0530

    working on proximity schemes

--- app/Http/Controllers/markingSchemeController.php
+++ app/Http/Controllers/markingSchemeController.php
@@ -4,16 +4,20 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\PastPupilMarkingScheme;
+use App\ProximityMarkingScheme;
 
 class markingSchemeController extends Controller
 {
     
     public function addProximityMarkingScheme(Request $request){
     	//
+    	$proximityMarking = new ProximityMarkingScheme();
+   		$proximityMarking->addMarkingScheme($request);
+   		return view('addMarkingScheme');
     }
 
     public function addPastPupilMarkingScheme(Request $request){
-   		$pastPupilMarking = new PastPupilMarkingScheme();
+   		$pastPupilMarking = new ProximityMarkingScheme();
    		$pastPupilMarking->addMarkingScheme($request);
    		return view('addMarkingScheme');
     }
