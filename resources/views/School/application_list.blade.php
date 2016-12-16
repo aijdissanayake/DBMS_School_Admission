410489851ebe928ae326aed245c73a4f9fc1e5e3
Achala Dissanayake <aijdissanayake@gmail.com>
2016-12-16 12:52:36 +0530

    SchoolController Created

--- resources/views/School/application_list.blade.php
+++ resources/views/School/application_list.blade.php
@@ -5,7 +5,10 @@
 @endsection
 
 @section('content')
-<h3>Let's see if this comes up!!</h3>
-
-<a href="{{route('newApplication')}}" role="button" class="btn btn-default">Enter Application</a>
+<h2>List Group With Badges</h2>
+  <ul class="list-group">
+    <li class="list-group-item">New <span class="badge">12</span></li>
+    <li class="list-group-item">Deleted <span class="badge">5</span></li>
+    <li class="list-group-item">Warnings <span class="badge">3</span></li>
+  </ul>
 @endsection
