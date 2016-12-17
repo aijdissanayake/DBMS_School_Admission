617aeb0abcf56807969866871ff4b393c963995f
Shane Wolff <Shane Wolff>
2016-12-17 09:30:27 +0530

    form tag in school_add.blade.php was edited

--- resources/views/school/school_add.blade.php
+++ resources/views/school/school_add.blade.php
@@ -8,7 +8,7 @@
 <h1>New School Registration</h1>
 <div class="row">
 	<div class="col-md-4">		
-	<form>
+	<form method="post", action="{{route('schooldAdd')}}">
 	{{csrf_field()}}
 		<div class="form-group">
 			<label for="regNum">School Registration Number</label>
