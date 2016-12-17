e3c5821c351ee2b9eb3f6bd62d7f282f9e528e06
Shane Wolff <Shane Wolff>
2016-12-17 09:03:48 +0530

    addNewRecord function was added into PastpupilController, form tag was edited in pastpupil_record_add.blade.php

--- app/Http/Controllers/PastPupilController.php
+++ app/Http/Controllers/PastPupilController.php
@@ -9,11 +9,20 @@ use Illuminate\Http\RedirectResponse;
 class PastPupilController extends Controller
 {
     public function addNew(Request $request) {
-
     	$nic = $request['nicNum'];
     	$name = $request['name'];
     	$pastpupil = new PastPupil();
     	$pastpupil->addPupil($nic, $name);
     	return redirect(route('newPastPupil'))->with('status', 'New past pupil was successfully added! You may continue adding more.');
+    }
+
+    public function addNewRecord(Request $request) {
+    	$nic = $request['nicNum'];
+    	$regNum = $request['regNum'];
+    	$years = $request['years'];
+    	$eduLevel = $request['edu_level'];
+    	$coCurLevel = $request['co_curricular_level'];
+    	$exCurLevel = $request['extra_curricular_level'];
+    	return redirect(route('newPastPupilRecord'))->with('status', 'Past pupil records were successfully added! You may continue adding more.');
     }
 }
--- resources/views/pastpupil/pastpupil_record_add.blade.php
+++ resources/views/pastpupil/pastpupil_record_add.blade.php
@@ -8,7 +8,7 @@
 <h1>New Past Pupil Record</h1>
 <div class="row">
 	<div class="col-md-4">		
-	<form>
+	<form method="post", action="{{route('pastpupilRecordAdd')}}">
 	{{csrf_field()}}
 		<div class="form-group">
 			<label for="nicNum">Registered NIC Number</label>
