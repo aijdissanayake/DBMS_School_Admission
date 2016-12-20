ba98506344d2578812b6854036b434f9ffc15a9e
Achala Dissanayake <aijdissanayake@gmail.com>
2016-12-20 22:21:38 +0530

    functionality-Application Search

--- routes/web.php
+++ routes/web.php
@@ -70,3 +70,4 @@ Route::get('/school/searchApps', 'SchoolController@searchSchoolApps')->name('sea
 Route::get('/applicationsHome', ['as' => 'applications', 'uses' => function () {
 	return view('applications');
 }]);
+Route::get('/school/applicationlist/{id}', 'ApplicationController@viewApp')->name('studentApp');
