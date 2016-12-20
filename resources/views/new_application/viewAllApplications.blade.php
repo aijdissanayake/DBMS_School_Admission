abd14e16fef7b11387bb5abf9d78b420107e45b4
Shane Wolff <Shane Wolff>
2016-12-21 00:40:04 +0530

    editing...

--- resources/views/new_application/viewAllApplications.blade.php
+++ resources/views/new_application/viewAllApplications.blade.php
@@ -8,6 +8,15 @@
 <div class="row">
 	<div class="col-md-12">
 		<h1>Current Application Submissions</h1>
+		<table class="table table-hover">
+			@foreach
+			<tr>
+				<td></td>
+				<td></td>
+				<td></td>
+			</tr>
+			@endforeach
+		</table>
 	</div>
 </div>
 
