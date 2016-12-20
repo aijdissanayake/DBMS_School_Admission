eed65bb57dde59eb0a8f8b40b6107631efb4b3d3
Achala Dissanayake <aijdissanayake@gmail.com>
2016-12-20 21:39:12 +0530

    functionality-Application Search

--- resources/views/School/application_list.blade.php
+++ resources/views/School/application_list.blade.php
@@ -7,23 +7,39 @@
 @endsection
 
 @section('content')
+
+
 <div class="row">
-	<div class="col-sm-8">
+	<div class="col-sm-3">
 	<h2 >{{$school_details->name}}</h2>
 	</div>
-	<div class="col-sm-4">
-	<form>
-
+	<div class="col-sm-9">
+		<div class="row">
+			<br><br>
+			<form action="{{route('')}}" method="post">
+			<div class="col-sm-5">
 			<div class="form-group">
-                <select type="text" class="form-control" placeholder="Search" id="field">
+                <select type="text" class="form-control" placeholder="Search" id="field" >
                 	<option value="denoted_name">first Name</option>
                 	<option value="surname" selected>Last Name</option>
                 </select>
             </div>
-            <div class="form-group">
-                <input type="text" class="form-control" placeholder="Search" id="search">
             </div>
-     </form>
+            <div class="col-sm-5">
+            <div class="form-group">
+                <input type="text" class="form-control" placeholder="Search" id="search" list="results">
+                <datalist id="results">
+				</datalist>
+            </div>
+            </div>
+            <div class="col-sm-2">
+            <div class="form-group">
+            <button class="form-control" type="submit"> Submit</button>
+            </div>
+            </div>
+            </form>
+        </div>
+            
      </div>
 </div>
 <div class="row">
