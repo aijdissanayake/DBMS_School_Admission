d29268296c96a323575cfefe03f676a189910bbc
Lahiru de Alwis <lahirud.14@cse.mrt.ac.lk>
2016-12-21 02:49:19 +0530

    editing marking schemes view

--- resources/views/addMarkingScheme.blade.php
+++ resources/views/addMarkingScheme.blade.php
@@ -67,4 +67,18 @@
 		@endif
 		</div>
 
+		<div class="row">
+			<div class="col-md-6">
+				<div class="panel panel-default">
+				  <div class="panel-heading">
+				    <h3 class="panel-title">Panel title</h3>
+				  </div>
+				  <div class="panel-body">
+				    Panel content
+				  </div>
+				</div
+			</div>
+
+		</div>
+
 @endsection
