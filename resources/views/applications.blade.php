460e779c0404b45438d568631ca3f01d490cb325
Lahiru de Alwis <lahirud.14@cse.mrt.ac.lk>
2016-12-20 14:43:07 +0530

    working on triggers

--- resources/views/applications.blade.php
+++ resources/views/applications.blade.php
@@ -4,8 +4,9 @@
 
 
 <div>
-	<a class="btn btn-primary btn-lg" href="{{route('newApplication')}}" role="button">Submit new application</a>
-	<a class="btn btn-primary btn-lg" href="#" role="button">View all applications</a>
+	<a class="btn btn-default btn-lg" href="{{route('newApplication')}}" role="button">Submit new application</a>
+	<a class="btn btn-default btn-lg" href="#" role="button">View all applications</a>
 </div>
 
 @endsection
+
