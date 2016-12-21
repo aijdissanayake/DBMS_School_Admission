dcc13aed8a57dcb58a635afe99770ad857861c45
Achala Dissanayake <aijdissanayake@gmail.com>
2016-12-21 07:19:15 +0530

    some routes

--- resources/views/applications.blade.php
+++ resources/views/applications.blade.php
@@ -6,7 +6,11 @@
 <div class="row">
 	<div class="col-xs-12 col-md-8 col-md-offset-2">
 		<a class="btn btn-primary btn-lg" href="{{route('newApplication')}}" role="button">Submit new application</a>
+		@if($user_name == 'admin')
 		<a class="btn btn-primary btn-lg" href="{{route('viewAllApp')}}" role="button">View all applications</a>
+		@else
+		<a class="btn btn-primary btn-lg" href="{{route('list')}}" role="button">View all applications</a>
+		@endif
 	</div>
 </div>
 
